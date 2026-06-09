ea86b74deb3d6c837d19a92f148524a171fbbbc6
area51balcarce-ctrl <area51.balcarce@gmail.com>
2026-06-08 22:46:13 -0300

    Delete api/health.js

--- api/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    service: 'AREA 51 Importador V6',
-    igdbConfigured: Boolean(process.env.TWITCH_CLIENT_ID && process.env.TWITCH_CLIENT_SECRET)
-  });
-};
